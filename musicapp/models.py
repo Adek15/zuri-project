b1e8dd4a24ccf3870596e27d4fac5f37528f309d
Adek15 <88534016+Adek15@users.noreply.github.com>
2022-12-11 15:23:58 +0100

    added to models

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    stage_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.stage_name
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=50)
+    date_released = models.DateField()
+    genre = models.CharField(max_length=50)
+    artist_id = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Lyrics(models.Model):
+    content = models.TextField()
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
